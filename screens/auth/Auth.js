f40400aa0cd37b877fda44fba505199506ffb964
Matheus ryuki Medeiros kaiya <dr_4_gao@hotmail.com>
2019-07-22 19:00:19 -0300

    Adicionado imagem de fundo

--- screens/auth/Auth.js
+++ screens/auth/Auth.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ImageBackground } from 'react-native';
 import { Text, Button, TextInput  } from 'react-native-paper';
+import BackgroundImage from '../../assets/download.jpg';
 
 class AuthScreen extends Component {
   static navigationOptions = {
@@ -22,38 +23,41 @@ class AuthScreen extends Component {
 
   render() {
     return (
+      <ImageBackground source={BackgroundImage} style={styles.backgroundImage}> 
       <View style={styles.container}>
-        <Text>Por favor, faça login!</Text>
-        <Button mode="contained">
-          Mudar para Login
-        </Button>
-        <View style={styles.inputContainer}>
-          <TextInput
-            mode='outlined'
-            label='Email'
-            style={styles.input}
-            value={this.state.email}
-            onChangeText={email => this.setState({ email })}
-          />
-          <TextInput
-            mode='outlined'
-            label='Senha'
-            style={styles.input}
-            value={this.state.senha}
-            onChangeText={senha => this.setState({ senha })}
-          />
-          <TextInput
-            mode='outlined'
-            label='Confirmar senha'
-            style={styles.input}
-            value={this.state.senha2}
-            onChangeText={senha2 => this.setState({ senha2 })}
-          />
-        </View>
-        <Button mode="contained" onPress={this.loginHandler}>
-          Login
-        </Button>
+          <Text style={styles.textHeading}>Por favor, faça login!</Text>
+          <Button mode="contained">
+            Mudar para Login
+          </Button>
+          <View style={styles.inputContainer}>
+            <TextInput
+              mode='outlined'
+              label='Email'
+              selectionColor="gray"
+              style={styles.input}
+              value={this.state.email}
+              onChangeText={email => this.setState({ email })}
+            />
+            <TextInput
+              mode='outlined'
+              label='Senha'
+              style={styles.input}
+              value={this.state.senha}
+              onChangeText={senha => this.setState({ senha })}
+            />
+            <TextInput
+              mode='outlined'
+              label='Confirmar senha'
+              style={styles.input}
+              value={this.state.senha2}
+              onChangeText={senha2 => this.setState({ senha2 })}
+            />
+          </View>
+          <Button mode="contained" onPress={this.loginHandler}>
+            Login
+          </Button>
       </View>
+      </ImageBackground>
     )
   }
 }
@@ -64,8 +68,18 @@ const styles = StyleSheet.create({
     alignItems: "center",
     flex: 1
   },
+  backgroundImage: {
+    width: '100%',
+    flex: 1,
+  },
   input: {
-    width: '100%'
+    width: '100%',
+    margin: 8,
+    backgroundColor: 'white'
+  },
+  textHeading: {
+    fontWeight: 'bold',
+    fontSize: 28,
   },
   inputContainer: {
     width: '80%'
